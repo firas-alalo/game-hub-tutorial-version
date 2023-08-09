98d9bc2e0685cbb7d9173c2d8e9c2a9878392694
Firas Alalo <Firas.vfx@gmail.com>
2023-08-09 04:09:08 +0200

    Filter games by genre

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -6,7 +6,7 @@ const ColorModeSwitch = () => {
   return (
     <HStack>
       <Switch
-        colorScheme="purple"
+        colorScheme="green"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
